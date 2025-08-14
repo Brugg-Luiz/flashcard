60cf86b3ff4ef25667de0b3139ceb4281f4efe9c
Brugg-Luiz <brugg.luiz@escola.pr.gov.br>
2025-08-14 08:27:31 -0300

    perguntas

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Português',
+    'O que é romantismo?',
+    'O Romantismo foi um movimento cultural, artístico e literário'
+)
+
+criaCartao(
+    'História',
+    'Quem ganhou a primeira Guerra Mundial?',
+    'Foi vencida pelas Potências Aliadas'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual o valor de x1 e x2? "2x² + 4x – 6 = 0"',
+    'x1 = -3, x2 = 1'
+)
+    criaCartao(
+    'Geografia',
+    'Em que continente fica a Papua-Nova Guiné?',
+    'A Papua-Nova Guiné se localiza na Oceania'
+)
